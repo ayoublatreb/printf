e92ed7fa5cde4aed6240d13b58425ba404882174
Ismail <thissismail@gmail.com>
2023-04-25 14:36:15 -0700

    Added char.c

---
+++ char.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_c - returns char as string
+ * @list: string
+ * Return: String version of char
+ */
+
+char *print_c(va_list list)
+{
+	char *s;
+	char c;
+
+	c = va_arg(list, int);
+
+	if (c == 0)
+		c = '\0';
+
+	s = malloc(sizeof(char) * 2);
+
+	if (s == NULL)
+		return (NULL);
+
+	s[0] = c;
+	s[1] = '\0';
+
+	return (s);
+}
